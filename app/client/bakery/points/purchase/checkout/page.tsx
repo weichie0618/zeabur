b3715328a25a214b1547303d043b5489c0a4e987
jeffy <r75050640@gmail.com>
2025-06-26 16:22:35 +0800

    0625

--- app/client/bakery/points/purchase/checkout/page.tsx
+++ app/client/bakery/points/purchase/checkout/page.tsx
@@ -519,19 +519,19 @@ export default function VirtualCardCheckoutPage() {
                   </div>
 
                   {/* 價格詳情 */}
-                  <div className="bg-gray-50 rounded-xl p-4 space-y-3">
+                
                    
                     <div className="border-t border-gray-200 pt-3">
                       <div className="flex justify-between items-center mb-2">
-                        <span className="text-lg font-medium text-gray-900">金額</span>
-                        <span className="text-2xl font-bold text-gray-900">NT$ {Math.round(item.price * item.quantity).toLocaleString()}</span>
+                        {/* <span className="text-lg font-medium text-gray-900">金額</span> */}
+                        <span className="text-2xl font-bold text-gray-900 justify-center">NT$ {Math.round(item.price * item.quantity).toLocaleString()}</span>
                       </div>
                       {/* <div className="flex justify-between items-center">
                         <span className="text-lg font-medium text-amber-700">獲得點數</span>
                         <span className="text-2xl font-bold text-amber-600">{(item.points_value * item.quantity).toLocaleString()} 點</span>
                       </div> */}
                     </div>
-                  </div>
+                  
                 </div>
               ))}
 
